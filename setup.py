6889ed9c87bc008e3586ac85c7dd493cb9a3604b
bailey everts <texas312@gmail.com>
2022-10-16 01:21:32 -0600

    ci: add semantic-release [#4]

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-from copyreg import constructor
-
 from setuptools import find_packages, setup
 
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="bails-route53-utils",
+    name="bails-aws-utils",
     version=__version__,
     license="MIT",
     author="Bailey Everts",
     author_email="me@baileyeverts.net",
-    description="For making route 53 easier",
-    url="https://github.com/beverts312/bails-route53-utils",
+    description="For making AWS easier",
+    url="https://github.com/beverts312/bails-aws-utils",
     packages=find_packages(),
     install_requires=["boto3", "python-crontab", "requests"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    scripts=["bin/dynamic-dns"],
+    scripts=[
+        "bin/dynamic-dns",
+    ],
 )
